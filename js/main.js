5152025c90de69609eb597d4ca80bc3f2d8193bd
David Gomez <dagove47@gmail.com>
2022-05-16 02:44:27 -0600

    Card character list added
    API fetched and card character list added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const dataResponse = document.getElementById("testDavid");
+const dataResponse = document.getElementById("cards-list");
 
 fetch('https://rickandmortyapi.com/api/character', {
     method: 'GET'
@@ -11,9 +11,17 @@ fetch('https://rickandmortyapi.com/api/character', {
     // dataResponse.innerHTML = characters;
     let characters = data.results;
     characters.forEach(character => {
-        console.log(character.name);
-        const info = `${character.name}<br>`;
-        dataResponse.innerHTML += info;
+        const card =    `<div class="card bg-dark m-1" style="width: 18rem;">
+                            <img src=${character.image} class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title fw-bold mb-3">${character.name}</h5>
+                                <p class="card-text">Specie: ${character.species}</p>
+                                <p class="card-text">Gender: ${character.gender}</p>
+                                <p class="card-text">Location: ${character.location.name}</p>
+                                <p class="card-text">Status: ${character.status}</p>
+                            </div>
+                        </div>`;
+        dataResponse.innerHTML += card;
     });
 })
 .catch(error => alert("ERROR"));
